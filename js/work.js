202e27a82cff1feea7408a33cdd6989bcce142f7
Andre Noack <herr.noack@gmx.de>
2017-02-02 17:48:33 +0100

    hover for work.html -> orange.html works !

--- js/work.js
+++ js/work.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#bild_work").mousemove(function(e) {
-		$('#bild_text').css('left', e.clientX + 20);
-		$('#bild_text').css('top', e.clientY + 5);
+		$('#bild_text').css('left', e.clientX + 10);
+		$('#bild_text').css('top', e.clientY + 0);
 		$('#bild_text').css('display', 'block');
 	});
 	$("#bild_work").mouseout(function(e) {

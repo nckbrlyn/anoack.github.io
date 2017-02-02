c7c66071ee4976318c8dc9fb921679be22681b3d
Andre Noack <herr.noack@gmx.de>
2017-02-02 16:56:38 +0100

    try to add a new slide – in progress (Inspiration from http://www.w3schools.com ~ I used the „try editor v3.3“ to understand the JS)

--- js/work.js
+++ js/work.js
@@ -2,9 +2,35 @@ $(document).ready(function() {
 	$("#bild_work").mousemove(function(e) {
 		$('#bild_text').css('left', e.clientX + 20);
 		$('#bild_text').css('top', e.clientY + 5);
-		$('#bild_text').css('display', 'inline');
+		$('#bild_text').css('display', 'block');
 	});
 	$("#bild_work").mouseout(function(e) {
 		$('#bild_text').css('display', 'none');
 	});
 })
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+  }
+  x[slideIndex-1].style.display = "block";
+}

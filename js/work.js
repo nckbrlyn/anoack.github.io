ffb63c48d6fc9f6559cd47f36ed0dfa846174494
Andre Noack <herr.noack@gmx.de>
2017-03-07 14:28:04 +0100

    something new – new navi

--- js/work.js
+++ js/work.js
@@ -27,3 +27,22 @@ document.images[0].src = "images/01_1_test.jpg"
 function bild_back () {
 document.images[0].src = "images/01_1.png"
 }
+
+// Add no-touch class to body for mobile touch events and toggle hover class on elements that need it
+	if ("ontouchstart" in document.documentElement) {
+		document.documentElement.className += " touch";
+	}
+
+	// Add and remove no-hover class to <li>'s for mobile hover events
+	jQuery('.touch #container').each(function() {
+		var div = jQuery(this);
+
+		div.hover(function() {
+			div.removeClass('no-hover');
+		});
+
+		jQuery('*').not(div).bind('click', function() {
+			div.addClass('no-hover');
+		});
+
+	});
